7338ba9424afbf8034cfcc8abd4365f2a1c27840
taqin <abdulmuttaqin456@gmail.com>
2023-10-07 14:55:44 +0700

    Update update_star_count.py

--- update_star_count.py
+++ update_star_count.py
@@ -2,7 +2,13 @@ import os
 import re
 from github import Github, GithubException
 
-def update_star_count(readme_contents, repo_info):
+def update_star_count(readme_contents, repo_info=None, repo_path=None, remove=False):
+    """Update star counts or remove the repository link."""
+    if remove:
+        # Remove the entire line referencing the repository
+        pattern = fr'.*\(https://github.com/{repo_path}\).*\n'
+        return re.sub(pattern, '', readme_contents)
+
     pattern = fr'\(⭐\s*\d+\)\s*\[{repo_info.name}\]'
     new_readme_line = f'(⭐ {repo_info.stargazers_count}) [{repo_info.name}]'
     return re.sub(pattern, new_readme_line, readme_contents)
@@ -22,13 +28,13 @@ def main():
             repo_info = gh.get_repo(repo_path)
             readme_contents = update_star_count(readme_contents, repo_info)
         except GithubException as e:
-            print(f"Error while fetching {repo_path}: {e}")
-            continue
+            print(f"Error while fetching {repo_path}: {e}")\
+            readme_contents = update_star_count(readme_contents, repo_path=repo_path, remove=True)
 
     if readme_contents != readme.decoded_content.decode("utf-8"):
         repo.update_file(
             path=readme.path,
-            message="Updated star counts",
+            message="Updated star counts and removed invalid repos",
             content=readme_contents,
             sha=readme.sha,
             branch="main",
